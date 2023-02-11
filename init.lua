3bc6714c73cba0b2398f7a18e276a51ead03daef
Zemtzov7 <72821250+zmv7@users.noreply.github.com>
2023-02-11 20:38:44 +0500

    Able to pay from ATM account to tp to bones

--- init.lua
+++ init.lua
@@ -7,11 +7,11 @@ core.register_on_dieplayer(function(player, reason)
 	lastdie[name] = vector.round(pos)
 end)
 
-core.register_privilege("tptobones","Allows to use /tptobones command")
+core.register_privilege("tptobones",{description="Allows to use /tptobones command",give_to_singleplayer=false})
 core.register_chatcommand("tptobones", {
   description = "TP you or player to last death pos",
   params = "[playername]",
-  privs = {tptobones=true},
+  privs = {interact=true},
   func = function(name, param)
 	if not param or param == "" then
 		param = name
@@ -27,6 +27,18 @@ core.register_chatcommand("tptobones", {
 	if not diepos then
 		return false, "Position of last death of "..param.." is not found"
 	end
-	player:set_pos(diepos)
-	return true, param.." teleported to last death pos "..core.pos_to_string(diepos)
+	if core.check_player_privs(name, {tptobones=true}) then
+		player:set_pos(diepos)
+		return true, param.." teleported to last death pos "..core.pos_to_string(diepos)
+	end
+	if type(atm) == "table" and atm.balance[name] then
+		if atm.balance[name] >= 1000 then
+			player:set_pos(diepos)
+			atm.balance[name] = atm.balance[name] - 1000
+			atm.saveaccounts()
+			return true, "You paid 1000 MG from ATM account and teleported to bones"
+		else
+			return false, "You dont have 1000 MG in ATM account for teleporting to bones"
+		end
+	end
 end})
